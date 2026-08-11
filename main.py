112f7573bc61150aed6777b9ca047f2555b95dd3
nexusaiproject-png <nexuslabs.ai.project@gmail.com>
2026-08-11 19:31:12 +0330

    feat: mount SaaS authentication API

--- main.py
+++ main.py
@@ -3,13 +3,10 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from app.api import router as tools_router
+from app.auth_api import router as auth_router
 from app.automation_api import router as automation_router
 from app.container import build_container
-from app.errors import (
-    permission_denied_handler,
-    tool_argument_error_handler,
-    tool_not_found_handler,
-)
+from app.errors import permission_denied_handler, tool_argument_error_handler, tool_not_found_handler
 from app.permissions import PermissionDeniedError
 from app.tools import ToolArgumentError
 from app.web import router as web_router
@@ -33,6 +30,7 @@ app.add_exception_handler(KeyError, tool_not_found_handler)
 app.add_exception_handler(ToolArgumentError, tool_argument_error_handler)
 app.include_router(tools_router)
 app.include_router(automation_router)
+app.include_router(auth_router)
 app.include_router(web_router)
 
 
